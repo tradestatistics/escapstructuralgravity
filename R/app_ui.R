880632ccb516a6c868a7180d106d741993fe49be
Mauricio 'Pacha' Vargas Sepulveda <m.sepulveda@mail.utoronto.ca>
2024-04-06 14:52:47 -0400

    TAB NAME

--- R/app_ui.R
+++ R/app_ui.R
@@ -311,7 +311,7 @@ golem_add_external_resources <- function() {
     favicon(),
     bundle_resources(
       path = app_sys("app/www"),
-      app_title = "POL2319"
+      app_title = "ESCAP SGD"
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
